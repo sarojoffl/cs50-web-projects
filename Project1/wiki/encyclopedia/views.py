8043d37a1d01288d32452f021dad4cf46583c316
Saroj <sarojoffl@gmail.com>
2024-02-06 23:41:44 +0545

    Implement Entry Page feature

--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 from . import util
-
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    content = util.get_entry(title)
+    if content is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "The requested page was not found."
+        })
+    else:
+        html_content = markdown2.markdown(content)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+        })
