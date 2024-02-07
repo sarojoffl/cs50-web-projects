46fc2e424ccaca264fa393b552d3e28a35091800
Saroj <sarojoffl@gmail.com>
2024-02-07 11:22:13 +0545

    Add search functionality

--- Project1/wiki/encyclopedia/urls.py
+++ Project1/wiki/encyclopedia/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
         path('wiki/<str:title>/', views.entry, name='entry'),
+        path('search', views.search, name='search'),
         path("", views.index, name="index")
 ]
--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . import util
 import markdown2
 
@@ -18,4 +20,17 @@ def entry(request, title):
         return render(request, "encyclopedia/entry.html", {
             "title": title,
             "content": html_content
+        })
+
+def search(request):
+    query = request.GET.get('q', '')
+    entries = util.list_entries()
+    matching_entries = [entry for entry in entries if query.lower() in entry.lower()]
+
+    if query in entries:
+        return HttpResponseRedirect(reverse('entry', kwargs={'title': query}))
+    else:
+        return render(request, "encyclopedia/search_results.html", {
+            "entries": matching_entries,
+            "query": query
         })
